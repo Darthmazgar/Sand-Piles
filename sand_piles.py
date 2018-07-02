ce75b7b4a66f36dcd523f6f1ce482d5d672e3437
Darthmazgar <34077975+Darthmazgar@users.noreply.github.com>
2018-07-02 17:01:52 +0100

    Added some function ideas

--- sand_piles.py
+++ sand_piles.py
@@ -1,6 +1,30 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 class Grid:
-	def __init__(self):
+	def __init__(self, MAX_SIZE, sand, square_size):
+		self.MAX_SIZE = MAX_SIZE
+		self.sc = sand
+		self.h = square_size
+		self.w = square_size
+
+	def get_index(self, x, y):
+		index = x + y * self.w
+		return index
+
+	def get_coords(self, index):
 		pass
+
+	def add_sand(self):
+		pass
+
+	def check_grid(self, grid):
+		pass
+
+	def display_grid(self):
+		pass
+
+def main():
+	grid = Grid(10, 10, 10)
+
+main()
